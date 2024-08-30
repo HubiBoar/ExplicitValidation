86b6b7a4e38b8235236b29cb8265563183090ffe
HubiBoar <150520704+HubiBoar@users.noreply.github.com>
2024-08-30 12:40:26 +0000

    Example for ValidObject

---
+++ src/Definit.Validation/SourceGeneratorTest/NewApproach.Object.cs
@@ -0,0 +1,120 @@
+using Definit.Results;
+
+namespace NewApproach;
+
+public sealed partial record UserData
+(
+    string Name,
+    Email Email,
+    Address Address
+)
+: IsValid;
+
+public sealed partial record Address
+(
+    string PostalCode,
+    Email Email
+)
+: IsValid;
+
+public static class ExampleObject
+{
+    private static async Task<Result> Endpoint(UserData body)
+    {
+        if(body.IsValid.Is(out Error error).Else(out var valid))
+        {
+            return error;
+        }
+
+        return await Run(valid);
+    }
+
+    private static async Task<Result> Run(UserData.Valid valid)
+    {
+        await Task.CompletedTask;
+        return Result.Success;
+    }
+}
+
+//Logic
+public abstract record IsValid
+{
+}
+
+//Auto generated
+public sealed partial record UserData
+{
+    public Result<Valid> IsValid => _idValid ??= Valid.IsValid(this);
+    private Result<Valid>? _idValid = null;
+
+    public sealed record Valid
+    (
+        string Name,
+        Email.Valid Email,
+        Address.Valid Address
+    )
+    {
+        public static implicit operator UserData(Valid value) => new
+        (
+            value.Name,
+            value.Email,
+            value.Address
+        );
+
+        public static Result<Valid> IsValid(UserData value)
+        {
+            List<Error> errors = new();
+            if(value.Email.IsValid.Is(out Error error).Else(out var validEmail))
+            {
+                errors.Add(error);
+            }
+
+            if(value.Address.IsValid.Is(out error).Else(out var validAddress))
+            {
+                errors.Add(error);
+            }
+
+            if(errors.Count > 0)
+            {
+                return new Error(string.Join(", ", errors.Select(x => x.Message)));
+            }
+            
+            return new Valid(value.Name, validEmail, validAddress);
+        }
+    }
+}
+
+public sealed partial record Address
+{
+    public Result<Valid> IsValid => _idValid ??= Valid.IsValid(this);
+    private Result<Valid>? _idValid = null;
+
+    public sealed record Valid
+    (
+        string PostalCode,
+        Email.Valid Email
+    )
+    {
+        public static implicit operator Address(Valid value) => new
+        (
+            value.PostalCode,
+            value.Email
+        );
+
+        public static Result<Valid> IsValid(Address value)
+        {
+            List<Error> errors = new();
+            if(value.Email.IsValid.Is(out Error error).Else(out var validEmail))
+            {
+                errors.Add(error);
+            }
+
+            if(errors.Count > 0)
+            {
+                return new Error(string.Join(", ", errors.Select(x => x.Message)));
+            }
+            
+            return new Valid(value.PostalCode, validEmail);
+        }
+    }
+}
---
+++ src/Definit.Validation/SourceGeneratorTest/NewApproach.Value.cs
@@ -0,0 +1,84 @@
+using Definit.Results;
+
+namespace NewApproach;
+
+public sealed partial record Email() : IsValid<string>(Rule.NotNull.Min(5));
+
+public static class ExampleValue
+{
+    private static async Task<Result> Endpoint(Email body)
+    {
+        if(body.IsValid.Is(out Error error).Else(out var valid))
+        {
+            return error;
+        }
+
+        return await Run(valid);
+    }
+
+    private static async Task<Result> Run(Email.Valid valid)
+    {
+        await Task.CompletedTask;
+        return Result.Success;
+    }
+}
+
+
+//Logic
+
+public sealed record Validator(Result Result)
+{
+    public Validator NotNull => this;
+
+    public Validator Min(int size) => this;
+}    
+
+public abstract record IsValid<TValue>(Validator Validator)
+{
+    public required TValue Value { get; init; }
+
+    protected Result Validate()
+    {
+        return Validator.Result;
+    }
+
+
+    protected static Validator Rule => new Validator(Result.Success);
+}
+
+
+//Auto generated
+public sealed partial record Email
+{
+    public Result<Valid> IsValid => _idValid ??= Valid.IsValid(this);
+    private Result<Valid>? _idValid = null;
+
+    public static implicit operator Email(string value) => new Email
+    {
+        Value = value,
+    };
+
+    public static implicit operator string(Email value) => value.Value;
+
+    public sealed record Valid
+    {
+        public string Value => Holder.Value;
+        private Email Holder { get; }
+
+        private Valid(Email holder)
+        {
+            Holder = holder;
+        }
+
+        public static implicit operator Email(Valid value) => value.Holder;
+
+        public static Result<Valid> IsValid(Email value)
+        {
+            if(value.Validate().Is(out Error error))
+            {
+                return error;
+            }
+            return new Valid(value);
+        }
+    }
+}

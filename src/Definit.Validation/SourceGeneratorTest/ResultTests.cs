01db79a695265cb72961798f1e81349230ccf936
HubiBoar <150520704+HubiBoar@users.noreply.github.com>
2024-09-07 17:50:05 +0000

    simplified result wip

--- src/Definit.Validation/SourceGeneratorTest/ResultTests.cs
+++ src/Definit.Validation/SourceGeneratorTest/ResultTests.cs
@@ -3,7 +3,7 @@ using static Definit.NewApproach.Result;
 
 namespace Definit.NewApproach;
 
-file static class Test
+file static partial class Test
 {
     private static int Run()
     {
@@ -33,35 +33,89 @@ file static class Test
         return 1;
     }
 
-    public static Result<string, NotFound> NewResult() => Try<string, NotFound>(c =>  
+    private static partial Result<string, NotFound> NewResult(Public _)  
     {
         var (j, not) = NewResult("test");
 
         if(j is null) return new NotFound(); 
 
-        return j.ToString();
-    },
-    ex => new NotFound());
+        return j.Value.ToString();
+    } 
 
-    public static Result<int, Not> NewResult(string value) => Try<int, Not>(c =>  
+    private static Result<int, Not> NewResult(Private _, string str)  
     {
-        var i = NewResult().Match(c, notFound => new Not());
-        return 0;
-    },
-    ex => new Not());
+
+        return str;
+    } 
+
+    //Auto generated
+    public static Either<string, Either<NotFound, Not>> NewResult()  
+    {
+        try
+        {
+            return NewResult(new ()).Value;
+        }
+        catch(Exception ex)
+        {
+            return NotFound.Create(ex); 
+        }
+    } 
+    //
+    //Auto generated
+    public static Either<int, Not> NewResult(string str)  
+    {
+        try
+        {
+            return NewResult(new (), str).Value;
+        }
+        catch(Exception ex)
+        {
+            return Not.Create(ex); 
+        }
+    } 
 }
 
 public interface IError<TSelf>
     where TSelf : IError<TSelf>
 {
-    public TSelf Create(Exception exception); 
+    public abstract static TSelf Create(Exception exception); 
 }
 
-public readonly struct NotFound();
-public readonly struct Not();
+public readonly struct NotFound() : IError<NotFound>
+{
+    public static NotFound Create(Exception exception) => new NotFound();
+}
+
+
+public readonly struct Not() : IError<Not>
+{
+    public static Not Create(Exception exception) => new Not();
+}
+
+public readonly record struct Error<T0, T1>(Either<T0, T1> Value) : IError<Error<T0, T1>>
+    where T0 : IError<T0>
+    where T1 : IError<T1>
+{
+    public void Deconstruct(out Null<T0>? t0, out Null<T1>? t1) => (t0, t1) = Value;
+
+    public static Error<T0, T1> Create(Exception exception)
+    {
+        return new (T0.Create(exception));
+    }
+
+    public static implicit operator Error<T0, T1>([DisallowNull] T0 value) => new (value);
+    public static implicit operator Error<T0, T1>([DisallowNull] T1 value) => new (value);
+    public static implicit operator Error<T0, T1>([DisallowNull] Null<T0> value) => new (value);
+    public static implicit operator Error<T0, T1>([DisallowNull] Null<T1> value) => new (value);
+    public static implicit operator Error<T0, T1>([DisallowNull] Null<T0>? value) => new (value!.Value);
+    public static implicit operator Error<T0, T1>([DisallowNull] Null<T1>? value) => new (value!.Value);
+}
 
 public static class Result
 {
+    public readonly struct Public;
+    public readonly struct Private;
+
     public sealed record NullError();
 
     public static readonly NullError Null = new (); 
@@ -76,51 +130,6 @@ public static class Result
             Value = value;
         }
     }
-
-    private static ForwardException<T> ForwardEx<T>([DisallowNull] T value) => new (value); 
-
-    public static Result<T0, T1> Try<T0, T1>
-    (
-        Func<Context<T0, T1>, Either<T0, T1>> func,
-        Func<Exception, Either<T0, T1>> onException
-    )
-    {
-        try
-        {
-            return new (func(Context<T0,T1>.Instance));
-        }
-        catch (ForwardException<T0> forwardT0)
-        {
-            return new (forwardT0.Value);
-        }
-        catch (ForwardException<T1> forwardT1)
-        {
-            return new (forwardT1.Value);
-        }
-        catch (Exception ex)
-        {
-            return new (onException(ex));
-        }
-    }
-
-    public sealed class Context<TC0, TC1>
-    { 
-        internal static readonly Context<TC0, TC1> Instance = new ();
-
-        private Context()
-        {
-        }
-
-        internal T0 Match<T0, T1>(Result<T0, T1> result, Func<T1, TC0> func)
-        {
-            return result.Match(t0 => throw ForwardEx(func(t0)!));  
-        }
-
-        internal T0 Match<T0, T1>(Result<T0, T1> result, Func<T1, TC1> func)
-        {
-            return result.Match(t0 => throw ForwardEx(func(t0)!));  
-        }
-    }
 }
 
 public static class Extensions
@@ -129,53 +138,31 @@ public static class Extensions
     {
         (t0, t1) = value.Value;
     }
-
-    public static T0 Forward<T0, T1>(this Result<T0, T1> value, Result.Context<T1,T0> context)  
-    {
-        return context.Match(value, v => v, e => e);
-    }
-
-    public static T0 Forward<T0, T1>(this Result<T0, T1> value, Result.Context<T0,T1> context)  
-    {
-        return context.Match(value, v => v, e => e);
-    }
-
-    public static T0 Forward<T0, T1, TC0, TC1>(this Result<T0, T1> value, Result.Context<TC0,TC1> context, Func<T1, TC0> func)  
-    {
-        return context.Match(value, func, e => e);
-    }
-
-    public static T0 Match<T0, T1, TC0, TC1>(this Result<T0, T1> value, Result.Context<TC0,TC1> context, Func<T1, TC1> func)  
-    {
-        return context.Match(value, func, e => e);
-    }
 }
 
-public sealed record Null<T>(T Value)
+public record struct Null<T>(T Value)
 {
     public static implicit operator T(Null<T> value) => value.Value;
     public static implicit operator Null<T>(T value) => new (value);
 }
 
-public sealed class Result<T0, T1>
+public readonly struct Result<T0, T1>
     where T1 : IError<T1>
 {
-    public Either<T0, T1> Value { get; }
+    internal Either<T0, T1> Value { get; }
 
     internal Result(Either<T0, T1> value)
     {
         Value = value;
     }
 
-    public void Deconstruct(out Null<T0>? t0, out Null<T1>? t1) => (t0, t1) = Value;
-
-    public T0 Match(Func<T1, T0> func1)
-    {
-    }
-
-    public TResult Match<TResult>(Func<T0, TResult> func0, Func<T1, TResult> func)
-    {
-    }
+    public static implicit operator Result<T0, T1>([DisallowNull] Result.NullError value) => new (value);
+    public static implicit operator Result<T0, T1>([DisallowNull] T0 value) => new (value);
+    public static implicit operator Result<T0, T1>([DisallowNull] T1 value) => new (value);
+    public static implicit operator Result<T0, T1>([DisallowNull] Null<T0> value) => new (value);
+    public static implicit operator Result<T0, T1>([DisallowNull] Null<T1> value) => new (value);
+    public static implicit operator Result<T0, T1>([DisallowNull] Null<T0>? value) => new (value!.Value);
+    public static implicit operator Result<T0, T1>([DisallowNull] Null<T1>? value) => new (value!.Value);
 }
 
 public record struct Either<T0, T1>

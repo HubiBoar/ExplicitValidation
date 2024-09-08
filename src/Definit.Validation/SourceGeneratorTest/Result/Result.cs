61f4e5e5c5d81443c9e0940539eb2b945425b194
HubiBoar <150520704+HubiBoar@users.noreply.github.com>
2024-09-08 22:09:48 +0000

    More genrator approach for result

--- src/Definit.Validation/SourceGeneratorTest/Result/Result.cs
+++ src/Definit.Validation/SourceGeneratorTest/Result/Result.cs
@@ -2,6 +2,55 @@ using System.Diagnostics.CodeAnalysis;
 
 namespace Definit.NewApproach;
 
+public interface IEither<T0, T1>
+    where T0: notnull
+    where T1: notnull
+{
+    public (Null<T0>?, Null<T1>?) Value { get; }
+
+    public void Deconstruct(out Null<T0>? t0, out Null<T1>? t1) => (t0, t1) = Value;
+
+    public interface IGenerator : Result.IGenerator
+    {
+        static string Result.IGenerator.Generate(string typeName) => $$"""
+        public record partial struct {{typeName}}<T0, T1> : IEither<T0, T1>
+            where T0 : notnull
+            where T1 : notnull
+        {
+            public (Null<T0>?, Null<T1>?) Value { get; }
+            
+            [Obsolete("Must not be used without parameters", true)]
+            public {{typeName}}() {}
+
+            public {{typeName}}(T0 value)
+            {
+                Value = (value, null);
+            }
+
+            public {{typeName}}(T1 value)
+            {
+                Value = (null, value);
+            }
+
+            public void Deconstruct(out Null<T0>? t0, out Null<T1>? t1) => (t0, t1) = Value;
+
+            public static implicit operator {{typeName}}<T0, T1>([DisallowNull] Result.NullError value) => throw new Exception(typeof(Either<T0,T1>).Name);
+            public static implicit operator {{typeName}}<T0, T1>([DisallowNull] T0 value) => new (value);
+            public static implicit operator {{typeName}}<T0, T1>([DisallowNull] T1 value) => new (value);
+            public static implicit operator {{typeName}}<T0, T1>([DisallowNull] Null<T0> value) => new (value);
+            public static implicit operator {{typeName}}<T0, T1>([DisallowNull] Null<T1> value) => new (value);
+            public static implicit operator {{typeName}}<T0, T1>([DisallowNull] Null<T0>? value) => new (value!.Value);
+            public static implicit operator {{typeName}}<T0, T1>([DisallowNull] Null<T1>? value) => new (value!.Value);
+        }
+        """;
+    }
+}
+
+[System.AttributeUsage
+(
+    System.AttributeTargets.Struct,
+    AllowMultiple = false
+)]
 public sealed class EitherAttribute<T0, T1> : Attribute, Result.IGenerator
     where T0: notnull
     where T1: notnull
@@ -39,6 +88,11 @@ public sealed class EitherAttribute<T0, T1> : Attribute, Result.IGenerator
     """;
 }
 
+[System.AttributeUsage
+(
+    System.AttributeTargets.Struct,
+    AllowMultiple = false
+)]
 public sealed class ResultAttribute<T0, T1> : Attribute, Result.IGenerator
     where T0: notnull
     where T1: notnull, IError<T1>
@@ -86,14 +140,46 @@ public sealed class ResultAttribute<T0, T1> : Attribute, Result.IGenerator
     """;
 }
 
-public interface IError<TSelf>
-    where TSelf : notnull, IError<TSelf>
-{
-    public abstract static TSelf Create(Exception exception); 
-}
-
 public static class Result
 {
+    public static string GetEitherCode(string typeName, string[] genericArguments)
+    {
+        var args = string.Join(", ", genericArguments);
+        var ret = $$"""
+        public record partial struct {{typeName}}<{{args}}> : IEither<T0, T1>
+            where T0 : notnull
+            where T1 : notnull
+        {
+            public (Null<T0>?, Null<T1>?) Value { get; }
+            
+            [Obsolete("Must not be used without parameters", true)]
+            public {{typeName}}() {}
+
+            public {{typeName}}(T0 value)
+            {
+                Value = (value, null);
+            }
+
+            public {{typeName}}(T1 value)
+            {
+                Value = (null, value);
+            }
+
+            public void Deconstruct(out Null<T0>? t0, out Null<T1>? t1) => (t0, t1) = Value;
+
+            public static implicit operator {{typeName}}<T0, T1>([DisallowNull] Result.NullError value) => throw new Exception(typeof(Either<T0,T1>).Name);
+            public static implicit operator {{typeName}}<T0, T1>([DisallowNull] T0 value) => new (value);
+            public static implicit operator {{typeName}}<T0, T1>([DisallowNull] T1 value) => new (value);
+            public static implicit operator {{typeName}}<T0, T1>([DisallowNull] Null<T0> value) => new (value);
+            public static implicit operator {{typeName}}<T0, T1>([DisallowNull] Null<T1> value) => new (value);
+            public static implicit operator {{typeName}}<T0, T1>([DisallowNull] Null<T0>? value) => new (value!.Value);
+            public static implicit operator {{typeName}}<T0, T1>([DisallowNull] Null<T1>? value) => new (value!.Value);
+        }
+        """;
+    }
+
+
+
     public interface IGenerator
     {
         public abstract static string Generate(string typeName);
@@ -114,16 +200,6 @@ public static class Result
     public static readonly NullError Null = new (); 
 }
 
-public static class Extensions
-{
-    public static void Deconstruct<T0, T1>(this Null<Either<T0, T1>> value, out Null<T0>? t0, out Null<T1>? t1)
-        where T0 : notnull
-        where T1 : notnull
-    {
-        (t0, t1) = value.Value;
-    }
-}
-
 public readonly struct Null<T>
     where T : notnull
 {
@@ -133,19 +209,17 @@ public readonly struct Null<T>
     public static implicit operator Null<T>(T value) => new() { Value = value };
 }
 
-public interface IEither<T0, T1>
-    where T0: notnull
-    where T1: notnull
-{
-    public (Null<T0>?, Null<T1>?) Value { get; }
-
-    public void Deconstruct(out Null<T0>? t0, out Null<T1>? t1) => (t0, t1) = Value;
-}
 
 public interface IResult<T0, T1>
     where T0 : notnull
     where T1 : notnull, IError<T1>
 {
+}
+
+public interface IError<TSelf>
+    where TSelf : notnull, IError<TSelf>
+{
+    public abstract static TSelf Create(Exception exception); 
 }
 
 public readonly struct Result<T0, T1> : IResult<T0, T1>
@@ -205,11 +279,6 @@ public record struct Either<T0, T1> : IEither<T0, T1>
     public Either(T1 value)
     {
         Value = (null, value);
-    }
-
-    internal Either((Null<T0>?, Null<T1>?) value)
-    {
-        Value = value;
     }
 
     public void Deconstruct(out Null<T0>? t0, out Null<T1>? t1) => (t0, t1) = Value;
